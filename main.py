a787200f22a30843ccc34081a3784f4940b05d4e
LUCKY FERNANDEZ HADUCA <147017329+HaducaLucky@users.noreply.github.com>
2024-10-25 18:56:39 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# name = input("Enter your full name: ")
+# phone_number = input("Enter you phone #: ")
+
+# result = len(name) #? to get the length of the string
+# result = name.find("M") #? to find
+# result = name.rfind("M") #? to find the last
+# name = name.capitalize() #? capitalize the first letter
+# name = name.upper() #? big letter all
+# name = name.lower() #? small letter all
+# result = name.isdigit() #? will return either True or False if the string contains digits
+# result = name.isalpha() #? will return either True or False if the string contains alphabetical letters
+# result = phone_number.count("-") #? to count
+# phone_number = phone_number.replace("-", " ") #? to replace any character
+
+print(help(str)) #* to show all string methods
+
+# print(phone_number)
